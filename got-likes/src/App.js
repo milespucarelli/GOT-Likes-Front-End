b3bfb85e9ca6af5ff35090af64c080f74e62b77c
milespucarelli <miles.pucarelli@gmail.com>
2019-03-05 15:38:16 -0500

    Pulling our changes

--- got-likes/src/App.js
+++ got-likes/src/App.js
@@ -3,27 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from "./components/NavBar";
 import Region from "./components/Region";
-import {Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import Footer from "./components/Footer"
+import Westeros from './components/Westeros'
 
 class App extends Component {
   render() {
     return (
-<<<<<<< HEAD
-
-    )
-=======
-      <div>
+      <div id="app">
         <NavBar />
         <Switch>
-          <Route path="/region" component={Region} />
-          <Route path="/" component={Home} />
+          <Route path='/map' component={Westeros} />
+          <Route path='/region' component={Region} />
+          <Route path='/' component={Home} />
         </Switch>
         <Footer />
       </div>
     );
->>>>>>> cc1ad5aed27ba2056bff299cccaaf4b28c18cb91
   }
 }
 
---
+++ got-likes/src/components/Westeros.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+class Westeros extends React.Component {
+  render() {
+    return <div id="map"></div>
+  }
+}
+
+export default Westeros
